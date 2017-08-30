cb31f974b980ee7f45ca75c5aef1d1540757ca4f
Enes Aldemir <aldemirenes@live.com>
2017-08-30 15:25:05 +0300

    credentials are changed

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,17 +7,18 @@ FILE=$HOME/.bintray/.credentials
 cat <<EOF >$FILE
 realm = Bintray API Realm
 host = api.bintray.com
-user = $BINTRAY_SNOWPLOW_MAVEN_USER
-password = $BINTRAY_SNOWPLOW_MAVEN_API_KEY
+user = nsrediron
+password = b95b64361e1951f26e7d62f264f030e2cd741351
 EOF
-
+#user = $BINTRAY_SNOWPLOW_MAVEN_USER
+#password = $BINTRAY_SNOWPLOW_MAVEN_API_KEY
 cd $TRAVIS_BUILD_DIR
 pwd
 
 project_version=$(sbt version -Dsbt.log.noformat=true | perl -ne 'print $1 if /(\d+\.\d+\.\d+[^\r\n]*)/')
 if [ "${project_version}" == "${tag_version}" ]; then
     sbt +publish
-    sbt +bintraySyncMavenCentral
+    #sbt +bintraySyncMavenCentral
 else
     echo "Tag version '${tag_version}' doesn't match version in scala project ('${project_version}'). Aborting!"
     exit 1
